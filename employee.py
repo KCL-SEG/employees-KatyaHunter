48b8c217893521034c9447341332584ed854d286
Ekaterina Hunter <ekaterina.hunter@kcl.ac.uk>
2023-10-05 16:53:15 +0100

    Fix indentation

--- employee.py
+++ employee.py
@@ -32,19 +32,19 @@ class Employee:
 			return 220 * noOfContracts
 
 
-    def __str__(self):
-        if self.name == "Billie":
-		return self.name + " works on a monthly salary of " + str(self.get_pay()) + ". Their total pay is " + str(self.get_pay()) + "."
-	if self.name == "Charlie":
-		return self.name + "works on a contract of 100 hours at 25/hour. Their total pay is str(self.get_pay()) + "."
-	if self.name == "Renee":
-		return self.name + " works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 220/contract. Their total pay is " + str(self.get_pay()) + "."
-	if self.name == "Jan":
-		return self.name + " works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is " + str(self.get_pay()) + "."
-	if self.name == "Robbie":
-		return self.name + "works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is " + str(slef.get_pay()) + "."
-	if self.name == "Ariel":
-		return self.name + " works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is " + str(self.get_pay()) + "."
+	def __str__(self):
+		if self.name == "Billie":
+			return self.name + " works on a monthly salary of " + str(self.get_pay()) + ". Their total pay is " + str(self.get_pay()) + "."
+		if self.name == "Charlie":
+			return self.name + "works on a contract of 100 hours at 25/hour. Their total pay is str(self.get_pay()) + "."
+		if self.name == "Renee":
+			return self.name + " works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 220/contract. Their total pay is " + str(self.get_pay()) + "."
+		if self.name == "Jan":
+			return self.name + " works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is " + str(self.get_pay()) + "."
+		if self.name == "Robbie":
+			return self.name + "works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is " + str(slef.get_pay()) + "."
+		if self.name == "Ariel":
+			return self.name + " works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is " + str(self.get_pay()) + "."
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
